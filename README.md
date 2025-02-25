8df2302305817261f3e4170ed62208dc8c77a091
majidH <majid.hadadian@gmail.com>
2025-02-25 11:17:36 +0330

    feat::Add Hero and HeroCarousel components

--- README.md
+++ README.md
@@ -1059,17 +1059,17 @@ import FeaturedProducts from '@/components/home/FeaturedProducts';
 import Hero from '@/components/home/Hero';
 import LoadingContainer from '@/components/global/LoadingContainer';
 import { Suspense } from 'react';
-function HomPage() {
-  return (
-    <>
-      <Hero />
-      <Suspense fallback={<LoadingContainer />}>
-        <FeaturedProducts />
-      </Suspense>
-    </>
-  );
-}
-export default HomPage;
+  function HomPage() {
+    return (
+      <>
+        <Hero />
+        <Suspense fallback={<LoadingContainer />}>
+          <FeaturedProducts />
+        </Suspense>
+      </>
+    );
+  }
+  export default HomPage;
 ```
 
 ### LoadingContainer Component
--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,6 +1,24 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import HeroCarousel from "./HeroCarousel";
+
 function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <section className="grid grid-cols-1 lg:grid-cols-2 gap-24  items-center">
+      <div>
+        <h1 className="text-4xl  tracking-tight max-w-2xl font-bold sm:text-6xl">
+          We are changing the way people shop
+        </h1>
+        <p className="mt-8 leading-8 text-lg text-muted-foreground">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit eius
+          numquam doloremque nam non cum quo placeat mollitia suscipit nisi!
+        </p>
+        <Button size='lg'  className="mt-10" asChild>
+          <Link href={'/prodcuts'}>Our Products</Link>
+        </Button>
+      </div>
+      <HeroCarousel />
+    </section>
+  );
 }
-export default Hero
+export default Hero;
