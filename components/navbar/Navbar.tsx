3aea5bf8a57e4cdc8fd8a8e08d60a431c8bb3163
majidH <majid.hadadian@gmail.com>
2025-03-05 10:17:23 +0330

    feat: add singleProductPage component

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -4,18 +4,21 @@ import CartButton from "./CartButton";
 import DarkMode from "./DarkMode";
 import LinksDropdown from "./LinksDropdown";
 import Container from "../global/Container";
+import { Suspense } from "react";
 function Navbar() {
   return (
     <nav className="border-b">
-    <Container className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-4 flex-wrap  py-8 ">
-      <Logo />
-      <NavSearch />
-      <div className="flex gap-4 items-center ">
-        <CartButton />
-        <DarkMode />
-        <LinksDropdown />
-      </div>
-    </Container>
+      <Container className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-4 flex-wrap  py-8 ">
+        <Logo />
+        <Suspense>
+          <NavSearch />
+        </Suspense>
+        <div className="flex gap-4 items-center ">
+          <CartButton />
+          <DarkMode />
+          <LinksDropdown />
+        </div>
+      </Container>
     </nav>
   );
 }
